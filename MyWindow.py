6e36ce40fe4c052c3ee14f76e5fc0eb59a905a1a
WShuo97 <1132584882@qq.com>
2021-10-22 23:47:24 +0800

    202110222347

--- MyWindow.py
+++ MyWindow.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtGui
+from PyQt5 import QtWidgets
 from PyQt5.QtCore import QUrl, pyqtSignal
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
@@ -10,81 +11,87 @@ import cv2
 
 
 class MyWindow(QMainWindow):
+    # new signal
     sendImg = pyqtSignal(int)
+    loadVid = pyqtSignal()
 
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
-
         loadUi("BaseWindow.ui", self)
 
-        # self.player = QMediaPlayer()
-        # self.videoPlayer = QVideoWidget(self.videoFrame)
-        # self.videoPlayer.setFixedSize(
-        #     self.videoFrame.width(), self.videoFrame.height())
-        # self.videoPlayer.show()
-        # self.player.setVideoOutput(self.videoPlayer)
-
-        self.selectVideo.clicked.connect(self.getVideo)
-        # self.player.positionChanged.connect(self.test)
-
+        # window settings
         self.showBlock.setFixedSize(
             self.showBlock.width(), self.showBlock.height())
-        # self.showBlock.setStyleSheet("QLabel{background:white;}"
-        #                              "QLabel{color:rgb(300,300,300,120);font-size:10px;font-weight:bold;font-family:宋体;}"
-        #                              )
 
-        self.image = []
+        # signal and slot
+        self.selectVideo.clicked.connect(self.getVideo)
+        self.selectDet.clicked.connect(self.getDet)
+        self.vidControl.clicked.connect(self.setPlayState)
+
         self.sendImg.connect(self.showImg)
+        self.loadVid.connect(self.vidPlay)
 
+        # others
+        self.images = [1 for _ in range(10)]
+        self.srcImages = []
+        self.ablePlay = False
         self.exit = False
-
-    def msg(self):
-        directory = QFileDialog.getOpenFileNames(
-            self, "选取文件", "./", "All Files (*);;Text Files (*.txt)")[0]
-        cap = cv2.VideoCapture(directory[0])
-
-        self.videoLength = cap.get(7)
-        self.videoFps = cap.get(5)
-
-        cap.release()
-        cv2.destroyAllWindows()
-        print(directory)  # ('D:/data/pythonProject/MyWindow.py', 'All Files (*)')
-
-        qMC = QMediaContent(QUrl(directory[0]))
-        self.player.setMedia(qMC)
-        self.videoPath.setText(directory[0])
-
-        # print(self.player.MediaStatus())
-        # print(self.player.position())
-
-        self.player.play()
+        self.stFrameIndex = 0
+        self.frameid = 0
 
     def getVideo(self):
         directory = QFileDialog.getOpenFileNames(
-            self, "选取文件", "./", "All Files (*);;Text Files (*.txt)")[0]
-        cap = cv2.VideoCapture(directory[0])
+            self, "Select File", "./", "All Files (*);;Text Files (*.txt)")[0]
+        self.vidPath = directory[0]
 
-        self.videoLength = cap.get(7)
-        self.videoFps = cap.get(5)
-        frameid = 0
-        self.videoLength = cap.get(7)
-        self.videoFps = cap.get(5)
-        blockTime = int(1./self.videoFps*1000)
-        while cap.isOpened():
-            _, frame = cap.read()
-            if frame is None or self.exit:
-                break
-            self.image.append(frame)
-            self.sendImg.emit(frameid)
-            frameid = frameid+1
+        cap = cv2.VideoCapture(self.vidPath)
+        # frameid = 0
+        self.videoLength = int(cap.get(7))
+        self.videoFps = int(cap.get(5))
+        self.blockTime = int(1./self.videoFps*1000)
+        self.size = (self.showBlock.width(), self.showBlock.height())
+        # while cap.isOpened():
 
-            cv2.waitKey(blockTime)
+        #     # while not self.ablePlay:
+        #     #     continue
+
+        #     _, frame = cap.read()
+        #     if frame is None:
+        #         break
+        #     self.images.append(frame)
+        #     # self.srcImages.append(frame)
+        #     # self.images.append(cv2.resize(frame, self.size))
+        #     print(frameid)
+        #     # self.sendImg.emit(frameid)
+        #     frameid = frameid+1
+
+        #     # cv2.waitKey(blockTime)
 
         cap.release()
         cv2.destroyAllWindows()
 
+        self.videoPath.setText(self.vidPath)
+
+        # self.loadVid.emit()
+
+    def getDet(self):
+        directory = QFileDialog.getOpenFileNames(
+            self, "Select File", "./", "All Files (*);;Text Files (*.txt)")[0]
+        self.detPath = directory[0]
+        self.detectorPath.setText(self.vidPath)
+
+    def setPlayState(self):
+        self.ablePlay = not self.ablePlay
+        if self.ablePlay:
+            self.vidControl.setText("暂停")
+            self.loadVid.emit()
+        else:
+            self.vidControl.setText("播放")
+
     def showImg(self, index):
-        shrink = cv2.cvtColor(self.image[index], cv2.COLOR_BGR2RGB)
+        # img = self.images[index]
+        img = cv2.resize(self.images[index], self.size)
+        shrink = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.QtImg = QtGui.QImage(shrink.data,
                                   shrink.shape[1],
                                   shrink.shape[0],
@@ -94,5 +101,31 @@ class MyWindow(QMainWindow):
         self.showBlock.setPixmap(QtGui.QPixmap.fromImage(self.QtImg))
         self.showBlock.show()
 
-    def test(self):
-        print(self.player.position(), "/", self.videoLength)
+    def vidPlay(self):
+        cap = cv2.VideoCapture(self.vidPath)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, self.frameid)  # 设置要获取的帧号
+        # self.frameid = 0
+        while(cap.isOpened()):
+            _, frame = cap.read()
+            ret = self.ablePlay
+            if self.exit:
+                break
+            if ret == True:
+                self.images[self.frameid % 10] = frame
+                self.sendImg.emit(self.frameid % 10)
+            else:
+                break
+            self.frameid = self.frameid+1
+            cv2.waitKey(self.blockTime)
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def closeEvent(self, event):
+        reply = QtWidgets.QMessageBox.question(
+            self, "Wraning!", "Really to quit?", QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            self.exit = True
+            event.accept()
+        else:
+            event.ignore()
